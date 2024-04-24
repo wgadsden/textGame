ee4c19364f3919aeef6ed4492f175777ffebe4a8
William Gadsden <106703809+wgadsden@users.noreply.github.com>
2024-04-24 15:45:08 -0400

    Add files via upload

---
+++ Text Game/main.py
@@ -0,0 +1,21 @@
+import combat as c, map as m, party as p
+import math, time, re
+
+# Input that removes all outside 
+# re.sub('\\W+','', input("> "))
+partyList = ["", "", "", ""]
+def main():
+    febbyfaber = p.character(1)
+    febbyfaber.join_party(partyList, 1)
+    chicchikin = p.character(0)
+    chicchikin.join_party(partyList, 0)
+    bonybuni = p.character(3)
+    bonybuni.join_party(partyList, 3)
+    foxifox = p.character(2)
+    foxifox.join_party(partyList, 2)
+    p.character.printChars()
+    
+
+
+if __name__ == "__main__":
+    main()
